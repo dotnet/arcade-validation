92b1429a252ed1e12fdf4c0f19c229484171f148
juanam <juanam@microsoft.com>
2018-12-11 14:10:04 -0800

    Rename to pull dependencies from Arcade SDK

---
+++ src/Validation/tests/BasicTests.cs
@@ -0,0 +1,71 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace Validation_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            Assert.True(true);
+        }
+    }
+}
