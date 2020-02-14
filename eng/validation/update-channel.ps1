588ec5bb0269bf8f3b9b15ed51fa4ddb1dff9c30
Cesar Soares Lucas <disoares@microsoft.com>
2020-02-13 17:13:28 -0800

    Wait until build promotion has happened. Dont fail pipeline if build is already in the channel.

--- eng/validation/update-channel.ps1
+++ eng/validation/update-channel.ps1
@@ -2,7 +2,8 @@ Param(
   [string] $maestroEndpoint,
   [string] $barToken,
   [string] $apiVersion = "2018-07-16",
-  [string] $targetChannelName = ".NET Eng - Latest"
+  [string] $targetChannelName = ".NET Eng - Latest",
+  [string] $azdoToken
 )
 
 . $PSScriptRoot\..\common\tools.ps1
@@ -12,6 +13,14 @@ function Get-Headers([string]$accept, [string]$barToken) {
     $headers.Add('Accept',$accept)
     $headers.Add('Authorization',"Bearer $barToken")
     return $headers
+}
+
+function Get-AzDO-Build([string]$token, [int]$azdoBuildId) {
+    $uri = "https://dev.azure.com/dnceng/internal/_apis/build/builds/${azdoBuildId}?api-version=5.1"
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":${token}"))
+    $headers = @{"Authorization"="Basic $base64AuthInfo"}
+    $content = Invoke-WebRequest -Uri $uri -Headers $headers -Method Get 
+    return $content | ConvertFrom-Json
 }
 
 $arcadeSdkPackageName = 'Microsoft.DotNet.Arcade.Sdk'
@@ -37,11 +46,40 @@ try {
 
     . $PSScriptRoot\..\common\darc-init.ps1
 
-    darc add-build-to-channel --id $buildId --channel "$targetChannelName"
-
+    $DarcOutput = darc add-build-to-channel --id $buildId --channel "$targetChannelName"
+    
     if ($LastExitCode -ne 0) {
         Write-Host "Problems using Darc to promote build ${buildId} to channel ${targetChannelName}. Stopping execution..."
+        Write-Host $DarcOutput
         exit 1
+	}
+
+    if ($DarcOutput -match "has already been assigned to") 
+    {
+        Write-Host "Build has already been assigned to channel '${targetChannelName}'."
+	}
+    else {
+	    $buildUrlRegex = "https://dnceng.visualstudio.com/internal/_build/results\?buildId=(?<buildId>[0-9]*)"
+
+	    $azdoBuildId = $DarcOutput | select-string -Pattern $buildUrlRegex -AllMatches | % { $_.Matches.Groups[1].Value } 
+        $waitIntervalsInSeconds = 60
+        $build = $null
+
+	    do {
+            Write-Host "Waiting ${waitIntervalsInSeconds} seconds for promotion build to complete... https://dnceng.visualstudio.com/internal/_build/results?buildId=${azdoBuildId}"
+
+            Start-Sleep -Seconds $waitIntervalsInSeconds
+
+            $build = Get-AzDO-Build -token $azdoToken -azdoBuildId $azdoBuildId
+	    } while ($build.status -ne "completed")
+
+        if ($build.result -eq "succeeded") {
+            Write-Host "The build was promoted with success."
+	    }
+        else {
+            Write-Host "Error trying to promote build. The promotion build finished with this result: $($build.result)"
+            exit 1
+        }    
 	}
 }
 catch {
