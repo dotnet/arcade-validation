be28059616a54086a7d8ac3dbcd3a773946b4473
Matt Mitchell <mmitche@microsoft.com>
2020-12-29 14:21:46 -0800

    Add a small infrastructure for running E2E tests in arcade-validation (#1988)
    * Add a small infrastructure for running E2E tests in arcade-validation
    Arcade-validation today is sorta a single test case. Build repo and ensure
    it passes. This change adds a small test framework that allows for on-the-fly
    creation of small new arcade repo layouts, building them, and checking results.
    
    It uses the following:
    - A optional common repo layout that avoids downloading the same packages and zips over and over
      over again. Instead, a small repo is created, restored, and then that repo .dotnet/.packages dirs
      are used in other tests.
    - A set of standard set-up methods in a builder to create typical basic repo layouts.
    
    The main goal of this is to avoid checking in as many files as possible. It only
    copies the eng/common dir from the parent arcade-validation repo.
    
    I added 5 test cases. 1 basic one and 4 focusing on error behavior in
    signing. https://github.com/dotnet/arcade/issues/6367. One of these actually
    fails (the post build sign one) today in these tests because they can't actually publish to AzDO (permissions of course)
    and the error condition for an empty post build sign list lives in the wrong place today.
    
    https://github.com/dotnet/arcade/pull/6656

---
+++ src/Validation/tests/CommonRepoResourcesFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation.Tests
+{
+    public class CommonRepoResourcesFixture : IDisposable
+    {
+        public RepoResources CommonResources { get; private set; }
+
+        public CommonRepoResourcesFixture()
+        {
+            CommonResources = RepoResources.Create(useIsolatedRoots: false).GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+            CommonResources.Dispose();
+        }
+    }
+}
---
+++ src/Validation/tests/RepoTests.cs
@@ -0,0 +1,163 @@
+﻿// Licensed to the .NET Foundation under one or more agreements. 
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using FluentAssertions;
+using Microsoft.Build.Utilities.ProjectCreation;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Validation.Tests
+{
+    [Trait("Category", "SkipWhenLiveUnitTesting")]
+    public class RepoTests : IClassFixture<CommonRepoResourcesFixture>
+    {
+        private CommonRepoResourcesFixture _commonRepoResourcesFixture;
+
+        public RepoTests(CommonRepoResourcesFixture commonResourcesFixture)
+        {
+            _commonRepoResourcesFixture = commonResourcesFixture;
+        }
+
+        [Fact]
+        public async Task BasicRepoBuild()
+        {
+            var envVars = Environment.GetEnvironmentVariables();
+            using (var builder = new TestRepoBuilder(nameof(BasicRepoBuild), _commonRepoResourcesFixture.CommonResources))
+            {
+                await builder.AddDefaultRepoSetupAsync();
+
+                builder.AddProject(ProjectCreator
+                    .Create()
+                    .PropertyGroup()
+                    .Property("AllowEmptySignList", "true"), "eng/Signing.props");
+
+                // Create a simple project
+                builder.AddProject(ProjectCreator
+                        .Templates
+                        .SdkCsproj(
+                            targetFramework: "net5.0",
+                            outputType: "Exe")
+                        .PropertyGroup()
+                        .Property("IsPackable", "true"),
+                    "./src/FooPackage/FooPackage.csproj");
+                await builder.AddSimpleCSFile("src/FooPackage/Program.cs");
+
+                builder.Build(
+                    TestRepoUtils.BuildArg("configuration"),
+                    "Release",
+                    TestRepoUtils.BuildArg("restore"),
+                    TestRepoUtils.BuildArg("sign"),
+                    TestRepoUtils.BuildArg("projects"),
+                    Path.Combine(builder.TestRepoRoot, "src/FooPackage/FooPackage.csproj"))
+                    .Should().NotThrow();
+            }
+        }
+
+        /// <summary>
+        /// We should get an error if AllowEmptySignList is set to false, or
+        /// if it is not set at all (default behavior), and there are no items to sign.
+        /// </summary>
+        /// <param name="propertyIsSet">Is the property set or are we using the expecte ddefault?</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task BuildShouldErrorIfNoItemsToSignAndNonEmptySignList(bool propertyIsSet)
+        {
+            var envVars = Environment.GetEnvironmentVariables();
+            using (var builder = new TestRepoBuilder(nameof(BuildShouldErrorIfNoItemsToSignAndNonEmptySignList), _commonRepoResourcesFixture.CommonResources))
+            {
+                await builder.AddDefaultRepoSetupAsync();
+
+                if (propertyIsSet)
+                {
+                    builder.AddProject(ProjectCreator
+                        .Create()
+                        .PropertyGroup()
+                        .Property("AllowEmptySignList", "false"), "eng/Signing.props");
+                }
+
+                // Create a simple project
+                builder.AddProject(ProjectCreator
+                        .Templates
+                        .SdkCsproj(
+                            targetFramework: "net5.0",
+                            outputType: "Exe")
+                        .PropertyGroup()
+                        .Property("IsPackable", "true"),
+                    "./src/FooPackage/FooPackage.csproj");
+                await builder.AddSimpleCSFile("src/FooPackage/Program.cs");
+
+                builder.Build(
+                    TestRepoUtils.BuildArg("configuration"),
+                    "Release",
+                    TestRepoUtils.BuildArg("restore"),
+                    TestRepoUtils.BuildArg("sign"),
+                    TestRepoUtils.BuildArg("projects"),
+                    Path.Combine(builder.TestRepoRoot, "src/FooPackage/FooPackage.csproj"))
+                    .Should().Throw<Exception>().WithMessage("*error : List of files to sign is empty. Make sure that ItemsToSign is configured correctly*");
+            }
+        }
+
+        /// <summary>
+        /// We should get an error if AllowEmptySignPostBuildList is set to false, or
+        /// if it is not set at all (default behavior), and there are no items to sign in post build signing
+        /// </summary>
+        /// <param name="propertyIsSet">Is the property set or are we using the expecte default?</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task BuildShouldErrorIfNoItemsToSignAndNonEmptySignPostBuildList(bool propertyIsSet)
+        {
+            var envVars = Environment.GetEnvironmentVariables();
+            using (var builder = new TestRepoBuilder(nameof(BuildShouldErrorIfNoItemsToSignAndNonEmptySignPostBuildList), _commonRepoResourcesFixture.CommonResources))
+            {
+                await builder.AddDefaultRepoSetupAsync();
+
+                // Always put in the AllowEmptySignList
+                var signingProps = ProjectCreator.Create().PropertyGroup();
+                signingProps.Property("AllowEmptySignList", "true");
+
+                if (propertyIsSet)
+                {
+                    signingProps.Property("AllowEmptySignPostBuildList", "false");
+                }
+
+                // Clear out ItemsToSignPostBuild
+                signingProps.ItemGroup()
+                    .ItemRemove("ItemsToSignPostBuild", "@(ItemsToSignPostBuild)");
+
+                builder.AddProject(signingProps, "eng/Signing.props");
+
+                // Create a simple project
+                builder.AddProject(ProjectCreator
+                        .Templates
+                        .SdkCsproj(
+                            targetFramework: "net5.0",
+                            outputType: "Exe")
+                        .PropertyGroup()
+                        .Property("IsPackable", "true")
+                        .Property("EnableSourceLink", "false"),
+                    "./src/FooPackage/FooPackage.csproj");
+                await builder.AddSimpleCSFile("src/FooPackage/Program.cs");
+
+                builder.Build(
+                    TestRepoUtils.BuildArg("configuration"),
+                    "Release",
+                    TestRepoUtils.BuildArg("restore"),
+                    TestRepoUtils.BuildArg("pack"),
+                    TestRepoUtils.BuildArg("publish"),
+                    TestRepoUtils.BuildArg("sign"),
+                    TestRepoUtils.BuildArg("projects"),
+                    Path.Combine(builder.TestRepoRoot, "src/FooPackage/FooPackage.csproj"),
+                    "/p:AutoGenerateSymbolPackages=false",
+                    "/p:PostBuildSign=true")
+                    .Should().Throw<Exception>($"build of repo {builder.TestRepoRoot} is post build signed")
+                    .WithMessage("*error : List of files to sign post-build is empty. Make sure that ItemsToSignPostBuild is configured correctly.*");
+            }
+        }
+    }
+}
