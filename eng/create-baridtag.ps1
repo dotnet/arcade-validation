344a165096ab853d7ee386a88a3870be1d33142a
Missy Messa <47990216+missymessa@users.noreply.github.com>
2021-11-15 14:51:58 -0800

    Add ValidatingBarIds tag to Arcade Validation builds (#2726)
    * Add bar id tag to builds
    
    * trying to fix the stage
    
    * fix yaml?
    
    * move some stuff around
    
    * why can't you find the script?
    
    * blerg
    
    * can't have : in tag name, apparently
    
    * try to repo not masking
    
    * Replacing removed steps
    
    * Cleaning up per code review feedback
    
    * Omit ValidatingBarIds tag for PRs
    
    * Added condition to run the Create BAR ID Tag, hope it works!

---
+++ eng/create-baridtag.ps1
@@ -0,0 +1,20 @@
+Param(
+  [Parameter(Mandatory=$true)][string] $azdoToken,
+  [Parameter(Mandatory=$true)][string] $barToken,
+  [Parameter(Mandatory=$true)][string] $githubPAT
+)
+
+set-strictmode -version 2.0
+$ErrorActionPreference = 'Stop'
+
+. $PSScriptRoot\common\tools.ps1
+$darc = & "$PSScriptRoot\validation\get-darc.ps1"
+
+$arcadeSdkPackageName = 'Microsoft.DotNet.Arcade.Sdk'
+$arcadeSdkVersion = $GlobalJson.'msbuild-sdks'.$arcadeSdkPackageName
+$assetData = & $darc get-asset --name $arcadeSdkPackageName --version $arcadeSdkVersion --github-pat $githubPAT --azdev-pat $azdoToken --password $bartoken --output-format json | convertFrom-Json
+
+## Get the BAR Build ID for the version of Arcade we are validating
+$barBuildId = $assetData.build.id
+
+Write-Host "##vso[build.addbuildtag]ValidatingBarIds $barBuildId"
