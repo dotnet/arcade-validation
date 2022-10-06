3e56cbc790063b7adb8158f96d1592a6955b5c67
Ricardo Arenas <riarenas@microsoft.com>
2022-10-06 12:55:22 -0700

    [Release/5.0] stop using hosted pool (with expired image) for validate publishing leg (#3423)
    * stop using hosted pool (with expired image) for validate publishing leg
    
    * move to vs2019 images + use netcoreapp3.1 darc + usebasicParsing

--- eng/validation/get-darc.ps1
+++ eng/validation/get-darc.ps1
@@ -1,5 +1,5 @@
 $path = "$PSScriptRoot\darc\$(New-Guid)"
 
-& $PSScriptRoot\..\common\darc-init.ps1 -toolpath $path | Out-Host
+& $PSScriptRoot\..\common\darc-init.ps1 -toolpath $path -darcVersion "1.1.0-beta.22220.1" | Out-Host
 
 return "$path\darc.exe"
